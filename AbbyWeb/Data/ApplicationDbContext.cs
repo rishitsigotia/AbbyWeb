3197f53f2940848244ffb93969e0dbbdc4a9f91b
Rishit <rsigotia123@gmail.com>
2023-07-16 15:28:13 +0530

    DataLayer Added

---
+++ AbbyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using AbbyWeb.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbbyWeb.Data
+{  
+    public class ApplicationDbContext: DbContext // it will be like data layer. It should include DbContext
+    {
+        public DbSet<Category> Category { get; set; }
+        
+    }
+}
