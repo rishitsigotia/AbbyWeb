c5a2c312452531965b672bb77c6bdc7c50422567
Rishit <rsigotia123@gmail.com>
2023-07-16 15:44:20 +0530

    Program.cs Database Settings

--- AbbyWeb/Data/ApplicationDbContext.cs
+++ AbbyWeb/Data/ApplicationDbContext.cs
@@ -5,6 +5,9 @@ namespace AbbyWeb.Data
 {  
     public class ApplicationDbContext: DbContext // it will be like data layer. It should include DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+        
         public DbSet<Category> Category { get; set; }
         
     }
