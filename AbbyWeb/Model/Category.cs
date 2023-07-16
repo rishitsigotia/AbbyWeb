d454471c11a9ebf6e9dfa4c5b17cc483021b7dbf
Rishit <rsigotia123@gmail.com>
2023-07-16 15:03:37 +0530

    Category Model Added

---
+++ AbbyWeb/Model/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace AbbyWeb.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
