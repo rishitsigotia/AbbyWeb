945c9eee65226156c89eec6a616638d64f11d705
Rishit <rsigotia123@gmail.com>
2023-07-19 00:28:17 +0530

    Annotations added (Display Name and Range)

--- AbbyWeb/Model/Category.cs
+++ AbbyWeb/Model/Category.cs
@@ -8,6 +8,8 @@ namespace AbbyWeb.Model
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Display(Name ="No. of Order")]
+        [Range(1,100,ErrorMessage ="The range should be 1 to 100 !! Not more than that")]
         public int DisplayOrder { get; set; }
     }
 }
