d8b522b8be298f8ac236d94dbdd49d94555f2303
Rishit <rsigotia123@gmail.com>
2023-07-18 13:30:48 +0530

    Server Side Validation added

--- AbbyWeb/Pages/Categories/Create.cshtml.cs
+++ AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -22,9 +22,13 @@ namespace AbbyWeb.Pages.Categories
         }
         public async Task<IActionResult> OnPost()
         {
-           await _db.Category.AddAsync(Category);
-            await _db.SaveChangesAsync();
-            return RedirectToPage("Index");
+            if(ModelState.IsValid)
+            {
+				await _db.Category.AddAsync(Category);
+				await _db.SaveChangesAsync();
+				return RedirectToPage("Index");
+			}
+           return Page();
         }
     }
 }
